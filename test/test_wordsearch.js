8465edf99e48a852b2d4001bfb2f695e7ed05ba5
b1u3too <almadsen792@live.ca>
2022-02-24 03:11:17 +0000

    implement wordsearch function locates horizonal and vertial words, forward and backward

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,95 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+  it("should return false if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'GOOBLE')
+
+    assert.isFalse(result);
+  });
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'YAAA')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present horizontally but backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'NERF')
+
+    assert.isTrue(result);
+  });
+  it("should return false if the word is not present horizontally but backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'POPSICLE')
+
+    assert.isFalse(result);
+  });
+  it("should return true if the word is present vertically but backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEST')
+
+    assert.isTrue(result);
+  });
+  it("should return false if the word is not present vertically but backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], // ['A'] ROW 0
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], // ['S', 'W'] ROW 1
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'], // ['Y', 'E', 'C'] ROW 2
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'], // [] ROW 3
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'TRUTHFULLY')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,88 @@
+// Feb 23, 2022 - LHL Pair Programming Exercise with Marina Bekker and Andrea Madsen
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    //join each row of letters into a string, and put those into a new array
+    // Check the horizontal array first, if it's true, end function, if false transpose the vertical rows into horizontal and re-use horizontal function to check again
+
+    //check horizontal
+    if(horizontalCheck(letters, word)) {
+        return true;
     }
+    //check vertical
+    if(horizontalCheck((transposeMatrix(letters)), word)) {
+        return true;
+    }
+    // check backwards horizontal
+    if(horizontalCheck(reverseMatrix(letters), word)) {
+        return true;
+    }
+    //check backwards vertical
+    if(horizontalCheck(reverseMatrix((transposeMatrix(letters))), word)) {
+        return true;
+    }
+    
+    // const diamondMatrix = getMatrixDiagonals(matrix);
+    // if (wordSearch(diamondMatrix)) {
+    //     return true;
+    // }
+
+    return false; 
+};
+
+//work in progress at diagonals stretch
+// const getMatrixDiagonals = function(matrix) {
+//     const totalOfColumns = matrix[0].length;
+//     const totalOfRows = matrix.length;
+//     const result = [];
+  
+//     for (let t = 0; t < totalOfRows + totalOfColumns - 1; t++) {
+//         result[t] = [];
+//         for (let i = 0; i < t + 1; i++) {
+//           if (i < totalOfColumns);
+//           // this will crash on diagonal 8
+//             result[t].push(matrix[t - i][i]);
+//         }
+//         console.log(result);
+//     }
+  
+//     return result;
+//   }
+
+// reverseMatrix will take in the matric, and for each row will reverse it. 
+const reverseMatrix = function (matrix) {
+    let resultMatrix = [];
+
+    for (const row of matrix) {
+        resultMatrix.push(row.reverse());
+    }
+
+    return resultMatrix;
 }
 
-module.exports = wordSearch
+const horizontalCheck = function (letters, word) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const r of horizontalJoin) {
+        if (r.includes(word)) {
+            return true;
+        } 
+    }
+
+return false;
+};
+
+const transposeMatrix = function (matrix) {
+    let resultMatrix = [];
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix[r].length; c++) {
+            // check if resultMatrix exists, if not, create it. Otherwise proceed and push the number into it.
+            if (!resultMatrix[c]) {
+            resultMatrix.push([]);
+            }
+            resultMatrix[c].push(matrix[r][c]);
+        }
+    }
+    return resultMatrix;
+};
+
+module.exports = wordSearch;
+
